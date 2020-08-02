4c68c3fc054c08372c19f2ed5cfa00444d7b96d1
Ram Tiwari <50477195+ramtw@users.noreply.github.com>
2020-08-02 08:24:31 +0530

    Add files via upload

---
+++ Projects/Python-Projects/calculator/test.py
@@ -0,0 +1,8 @@
+import test1 as t
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+print(t.add(a,b))
+print(t.sub(a,b))
+print(t.mul(a,b))
+print(t.div(a,b))
+print("random number generated was: ",t.a)
---
+++ Projects/Python-Projects/calculator/test1.py
@@ -0,0 +1,10 @@
+import random
+def add(a,b):
+    print(a,"+",b,"=",a+b)
+def sub(a,b):
+    print(a,"-",b,"=",a-b)
+def mul(a,b):
+    print(a,"*",b,"=",a*b)
+def div(a,b):
+    print(a,"/",b,"=",a/b)
+a=random.randint(10,1000)
